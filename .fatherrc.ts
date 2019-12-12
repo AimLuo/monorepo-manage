ca9f1579783aa7e3aa7df31ca54e6bff1d4c8a8d
AimLuo <aimluo@outlook.com>
2019-12-12 16:57:03 +0800

    chore(all): 增加测试打包顺序

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -5,6 +5,8 @@ const options: IBundleOptions = {
   esm: 'rollup',
   // @ts-ignore
   doc: { typescript: true, src: 'packages/' },
+  // 这种方式打包会报错，所以无解https://github.com/umijs/father/issues/175#L195
+  // pkgs: ['@demo/umi-origin-test', '@demo/my-test'],
 };
 
 export default options;
